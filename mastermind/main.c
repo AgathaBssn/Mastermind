3f940ebc8e86f65431745b4d0834290c30fc72dd
AgathaBssn <agatha.basson@gmail.com>
2025-11-27 11:02:26 +0100

    feat: uses ihm logic

--- mastermind/main.c
+++ mastermind/main.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include "combo.h"
+#include "display.h"
 
 int main(void) {
-    Combo x = {
+    int stillPlaying = 0;
+
+    stillPlaying = askToPlay();
+
+    if (stillPlaying) {
+        Combo x = {
         { COLOR_UNDEF, COLOR_UNDEF, COLOR_UNDEF, COLOR_UNDEF },
         { SOL_UNCHECKED, SOL_UNCHECKED, SOL_UNCHECKED, SOL_UNCHECKED }
-    };
+        };
 
-    showCombo(&x);
+        showCombo(&x);
 
-    x.attempt[0] = COLOR_R;
-    x.attempt[1] = COLOR_G;
-    x.attempt[2] = COLOR_B;
-    x.attempt[3] = COLOR_Y;
+        x.attempt[0] = COLOR_R;
+        x.attempt[1] = COLOR_G;
+        x.attempt[2] = COLOR_B;
+        x.attempt[3] = COLOR_Y;
 
-    x.result[0] = SOL_PARTIAL;
-    x.result[1] = SOL_CORRECT;
+        x.result[0] = SOL_PARTIAL;
+        x.result[1] = SOL_CORRECT;
 
-    showCombo(&x);
+        showCombo(&x);
+    }
+    
 
     return 0;
 }
